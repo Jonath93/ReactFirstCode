4b2927ba31a1a449996441cb4ed523898e2e179e
Jonathan Benitez <jonaben93@gmail.com>
2019-07-26 10:12:51 -0700

    nativeInit

---
+++ HelloWorldApp.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+import {Text,View} from 'react-native'
+
+export default class HelloWorldApp extends Component {
+    render(){
+        return(
+            <view>
+                <text>Hello World !</text>
+            </view>
+        );
+    }
+}
